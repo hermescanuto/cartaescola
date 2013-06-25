c80314c94b44aa662c314ff459a1da4b28c2e1dd
Hermes <hermes.canuto@gmail.com>
2013-06-25 17:11:02 -0300

    Expediente atualizado

--- application/views/front/expediente.php
+++ application/views/front/expediente.php
@@ -50,126 +50,88 @@
 					</h2>
 
 
-					<p>
-						<strong>Carta na Escola</strong><br> <strong>ISSN 1808–6012</strong>
-					</p>
-					<p>
-						<strong>Diretor de Redação:</strong> Mino
-						Carta<br> <strong>Redatora-chefe:</strong> Lívia Perozim<br> <strong>Consultor
-							editorial: </strong>Luiz Gonzaga Belluzzo<br> <strong>Editor
-							responsável:</strong> Sergio Lirio
-					</p>
-					<p>
-						<strong>Consultora pedagógica:</strong> Maria José Nóbrega<br>
-						<strong>Editora-assistente:</strong> Clarice Cardoso<br> <strong>Repórter:
-						</strong>Tory Oliveira<br> <strong>Estagiária: </strong>Thais
-						Paiva<br> <strong>Colaboradores:</strong> Antonio Luiz M. C.
-						Costa, Caeto, Carlos Fico, Edilson Adão, Eric Nepomuceno, Fabiana
-						Marini Braga, Fábio Konder Comparato, Gustavo Vara, Ivam Cabral,
-						Jonas Santos, José Carlos de Souza, Leandro Almeida, Marcos
-						Méndez, Orlando Margarido, Roberto Taddei, Sírio Possenti,
-						Thalma Freitas, Vera Carolina Longo, Verônica Manevy, William
-						Vieira<br> <strong>Revisão:</strong> Ali Onaissi (coordenação),
-						Hassan Ayoub, Rita Leite
-					</p>
-					<p>
-						<strong>Projeto gráfico: </strong>Equipe CartaCapital<br> <strong>Diretora
-							de arte: </strong>Pilar Velloso<br> <strong>Chefe de arte:</strong>
-						Estella Maris, George B. J. Duque Estrada (Fundador) <br> Mariana
-						Ochs (Projeto original) <br> <strong>Inovação digital:</strong>
-						Adriana Corradi (Editora), Daniela Neiva (Subeditora). <br>
-						Produção gráfica/digital: Reinaldo Belintani<br> <strong>Programador:
-						</strong>Hermes Canuto<br> <strong>Designers:</strong> Ana Beatriz
-						Pádua (Infográficos), Lidiane Siqueira (Digital), Milena Branco,
-						Regina de Assis (Digital) e Marcos Medeiros (Assitente) <br> <strong>Fotografia:</strong>
-						Mara Lúcia da Silva (Chefe de Produção), Gabriela Giordano
-						(Produtora Editorial) e Pedro Presotto (Assistente de Fotografia)<br>
-						<strong>Secretárias:</strong> Catia Praxedes e Ingrid Sabino
-					</p>
-					<p>
-						Site: <a href="http://www.cartacapital.com.br/carta-na-escola"
-							target="_blank">www.cartacapital.com.br/carta-na-escola</a>
-					</p>
-					<p>
-						<strong>Conselho editorial:</strong> Antonio Delfim Netto, Eduardo
-						Rocha Azevedo, <br> Luiz Gonzaga Belluzzo, Manuela Carta e Mino
-						Carta (presidente)
-					</p>
-					<p>
-						<strong>Publisher: </strong>Manuela Carta<br> <strong>Gerente-Geral:</strong>
-						Luis Antonio Moraes
-					</p>
-					<p>
-						<strong>Publicidade</strong><br> <strong>Diretor de publicidade: </strong>Adriano
-						Chrisostomo<br> <strong>Venda de publicidade:</strong> Marielle
-						Brust (11) 3474-0181<br> <strong>Gerente de marketing:</strong>
-						Patricia Grosso<br> <strong>Coordenadora de marketing:</strong>
-						Daniela Mancuso<br> <strong>Assistentes de marketing: </strong>Pedro
-						Veríssimo Fernandes e Tatiana Giovani Silva<br> <strong>Gerente
-							de Negocios :</strong> José Rogério Biondo<br> <strong>Assistente
-							Administrativo:</strong> Claudinéia da Cruz<br> <strong>Assistente
-							de publicidade:</strong> Erika Viana Costa<br> <strong>E-mail:</strong>
-						<a href="mailto:comercial@cartacapital.com.br">comercial@cartacapital.com.br</a>
-					</p>
-					<p>
-						<strong>Representantes regionais:</strong><br> <strong>Rio de
-							Janeiro:</strong> Enio Santiago/Fernando Silva, (21)
-						2556-8898/2245-8660, enio@gestaodenegocios.com.br ,
-						fernando@gestaodenegocios.com.br<br> <strong>Brasília/DF: </strong>Charles
-						Marar/Charles Marar Filho, (61) 3321-0305, cmarar@uol.com.br
-						,espacom2@uol.com.br,<br> <strong>BA/AL/PE/SE: </strong>Canal C
-						Comunicação, (71) 3025-2670 Carlos Chetto, (71) 9617-6800/Luiz
-						Freire, (71) 9617-6815 canalc@canalc.com.br<br> <strong>Espírito
-							Santo:</strong> Enio Santiago/ enio@gestaodenegocios.com.br
-						Flávio Castro (27) 3389-3452/flavio@gestaodenegocios.com.br<br> <strong>Minas
-							Gerais: </strong>Marco Aurélio Maia (31)3281-0283/9983-2987
-						marcoaureliomaia@marcoaureliomaia.com<br> <strong>Goiás:</strong>
-						Cleonice M.P. Martins (62) 252-1313/252-1923/nicegm@terra.com.br<br>
-						<strong>Santa Catarina:</strong> Milton Marcucci/Ademir Gondran
-						(48) 333-8497, mg@matrix.com.br<br> <strong>Pará: </strong>Glícia
-						Diocesano (91) 3242-3367, gliciadiocesano@yahoo.com.br<br> <strong>CE/PI/MA/RN:
-						</strong>AG Holanda Comunicação (85) 3224-2267/
-						agholanda@agholanda.com.br<br> <strong>Rio Grande do Sul:</strong>
-						Inovação Soluções em Mídia Ltda., (51) 3021-7103,
-						contato@inovacaointel.com.br<br> <strong>Paraná: </strong>SEC
-						Estratégias (41) 3019-3717 Thadeu (41) 9926-0563,
-						thadeu@sec-estrategias.com.br Mara (41) 9926-0563,
-						mara@sec-estrategias.com.br
-					</p>
-					<p>
-						<strong>Gerente de circulação: </strong>Rita Teixeira<br> <strong>Coordenador
-							de circulação:</strong> Claudio Macias<br> <strong>Assistentes
-							de circulação - Assinaturas:</strong> Leonardo Borgiani e
-						Luciana Cristina Silva <br> <strong>Assistente de circulação -
-							Venda Avulsa:</strong> Juliana Augusta Pelizzon de Morais
-					</p>
-					<p>
-						<strong>Diretora administrativa/Financeira:</strong> Renata R. dos
-						Santos<br> <strong>Coordenadora de RH:</strong> Elisa Cavalini
-						Berenguel<br> <strong>Coordenador administrativo/Financeiro:</strong>
-						Mario Yamanaka <br> <strong>Equipe administrativa: </strong>Fabiana
-						Lopes Santos, Juliana Torres Corrêa , Eduardo Lucchesi e Maria
-						Aparecida da Rocha Calixto
-					</p>
-					<p>
-						<strong>Assessoria Contábil, Fiscal e Trabalhista: Escritório
-							Asteca de Contabilidade Ltda.</strong><br> Rua Siqueira Campos,
-						243 – Centro – Birigui – CEP 16200-055 – e-mail:
-						pedro@astecanet.com.br www.astecanet.com.br – (18) 3643-1228
-					</p>
-					<p>
-						Carta na Escola é uma publicação mensal da Editora Confiança
-						Ltda. <br> Carta na Escola não se responsabiliza pelos conceitos
-						emitidos nos artigos assinados. As pessoas que não constarem do
-						expediente não têm autorização para falar em nome de Carta na
-						Escola ou para retirar qualquer tipo de material se não
-						possuírem, em seu poder, carta em papel timbrado assinada por
-						qualquer pessoa que conste do expediente. Registro nº 179.584 de
-						23/8/94, modificado pelo registro nº 219.316 de 30/4/2002 no 1º
-						Cartório, de acordo com a Lei de Imprensa. Impressão: Gráfica
-						Plural - Distribuição: Fernando Chinaglia
-					</p>
-				</div>
+					<p> <strong>Carta na Escola</strong><br>
+                    <strong>ISSN 1808–6012</strong></p>
+                    <p> <strong>Diretor de Redação:</strong> Mino Carta<br>
+                      <strong>Redatora-chefe:</strong> Lívia Perozim<br>
+                      <strong>Consultor editorial: </strong>Luiz Gonzaga Belluzzo<br>
+                      <strong>Editor responsável:</strong> Sergio Lirio </p>
+                    <p> <strong>Consultora pedagógica:</strong> Maria José Nóbrega<br>
+                      <strong>Editora-assistente:</strong> Clarice Cardoso<br>
+                      <strong>Repórter: </strong>Tory Oliveira<br>
+                      <strong>Estagiária: </strong>Thais Paiva</p>
+                    <strong>EDITORES:</strong> Antonio Luiz Monteiro Coelho da Costa, paulo mota     (Projetos Especiais), Luiz Antonio Cintra,  Nirlando Beirão  e     Rosane Pavam<br>
+                    <strong>REPÓRTERES:</strong> ana ferraz, rodrigo martins, samantha maia e     willian Vieira<br>
+                    <strong>BRASÍLIA:</strong> André Barrocal, Cynara Menezes e Leandro Fortes<br>
+                    <strong>PARIS: </strong>Gianni Carta<br>
+                    <strong>Diretora de arte:</strong> Pilar Velloso <br>
+                    <strong>Chefe de arte: </strong>Estella Maris, George B. J. Duque Estrada     (Fundador) Mariana Ochs (Projeto original)<br>
+                    <strong>Designers:</strong> Ana Beatriz Pádua (Infográficos), milena branco e      Marcos Medeiros (Assistente)<br>
+                    <strong>Fotografia:</strong> Mara Lúcia da Silva (Chefe de Produção), Gabriela     Giordano (Produtora Editorial) e Pedro Presotto (auxiliar de     Fotografia) <br>
+                    <strong>Revisores:</strong> Áli Onaissi (coordenação), Hassan Ayoub e Rita     Leite<br>
+Produção Gráfica/digital: Reinaldo Belintani<br>
+<strong>SECRETÁRIA:</strong> Catia Praxedes e Ingrid Sabino<br>
+<br>
+<strong>Carta on-line</strong><br>
+<strong>Editor:</strong> Lino Bocchini<br>
+<strong>Editores assistentes:</strong> José Antonio Lima e Matheus Pichonelli<br>
+<strong>Repórteres: </strong>Gabriel Bonis, Marsílea Gombata e Piero     Locatelli<br>
+<strong>Estagiários:</strong> Maria Clara Parada, Paloma Rodrigues e Ricardo     Rossetto<br>
+<strong>Inovação digital:</strong> Adriana Corradi (Editora), Daniela Neiva     (Subeditora) <br>
+<strong>Designers: </strong>Lidiane Siqueira (digital) e Regina de Assis     (Digital)<br>
+<strong>Programador:</strong> Hermes Canuto<br>
+<strong>SITE: <a href="http://www.cartacapital.com.br">www.cartacapital.com.br</a></strong><br>
+<br>
+<strong>Conselho Editorial: </strong>antonio delfim netto, eduardo rocha     azevedo, luiz gonzaga belluzzo, manuela carta e mino carta     (presidente)<br>
+<strong>PUBLISHER:</strong> Manuela Carta<br>
+<strong>Gerente-Geral: </strong>Luis Antonio Moraes<br>
+<br>
+<strong>PUBLICIDADE</strong><br>
+<strong>DIRETOR DE PUBLICIDADE:</strong> Adriano Chrisostomo<br>
+<strong>GERENTE DE MARKETING:</strong> Patricia Grosso<br>
+<strong>coordenadora DE MARKETING:</strong> Daniela Mancuso<br>
+<strong>Analista de marketing:</strong> tatiana giovani silva<br>
+<strong>Assistente de marketing:</strong> Pedro Veríssimo Fernandes   <br>
+<strong>ASSISTENTE DE administrativo:</strong> Claudinéia da Cruz<br>
+<strong>VENDA DE PUBLICIDADE:</strong> Gerentes-executivos de Negócios:     Alexandre Carneiro, José Rogério Biondo, MAria Isabel Mandia,     Marielle Brust e Marlene Torres<br>
+assistente DE PUBLICIDADE: Erika Viana Costa<br>
+<strong>E-MAIL: </strong><a href="mailto:comercial@cartacapital.com.br">comercial@cartacapital.com.br</a><br>
+<strong>REPRESENTANTES REGIONAIS DE PUBLICIDADE:</strong><br>
+<strong>RIO DE JANEIRO:</strong> Enio Santiago/Fernando Silva,<br>
+(21) 2556-8898/2245-8660, <a href="mailto:enio@gestaodenegocios.com.br">enio@gestaodenegocios.com.br</a><br>
+<a href="mailto:fernando@gestaodenegocios.com.br">fernando@gestaodenegocios.com.br</a><br>
+<strong>BA/AL/PE/SE:</strong> Canal C Comunicação, (71) 3025-2670 – Carlos     Chetto, (71) 9617-6800/Luiz Freire, (71) 9617-6815 <a href="mailto:canalc@canalc.com.br">canalc@canalc.com.br</a><br>
+<strong>Brasília (DF):</strong> VertMídia. (61) 4063-7183 – Solange Farias. <br>
+<a href="mailto:solfarias@vertmidia.com.br">solfarias@vertmidia.com.br</a> <br>
+<strong>ESPÍRITO SANTO:</strong>  Enio <a href="mailto:Santiago/enio@gestaodenegocios.com.br">Santiago/enio@gestaodenegocios.com.br</a> Flávio Castro (27) <a href="mailto:3389-3452/flavio@gestaodenegocios.com.br">3389-3452/flavio@gestaodenegocios.com.br</a><br>
+<strong>MINAS GERAIS:</strong> Marco Aurélio Maia, (31) 3281-0283/9983-2987     marco<br>
+<a href="mailto:aureliomaia@marcoaureliomaia.com">aureliomaia@marcoaureliomaia.com</a><br>
+<strong>SANTA CATARINA:</strong>Marcucci&amp;Gondran Associados (48) 333-8497 <br>
+<a href="mailto:marcucci.gondran@terra.com.br">marcucci.gondran@terra.com.br</a><br>
+<strong>PARÁ:</strong> Glícia Diocesano, (91) 3242-3367, <a href="mailto:gliciadiocesano@yahoo.com.br">gliciadiocesano@yahoo.com.br</a><br>
+<strong>CE/PB/PI/MA/RN:</strong> AG Holanda Comunicação, (85) 3224-2267<br>
+<a href="mailto:agholanda@agholanda.com.br">agholanda@agholanda.com.br</a><br>
+<strong>RIO GRANDE DO SUL:</strong> Inovação Soluções em Mídia Ltda., (51)     3021-7103, <a href="mailto:contato@inovacaointel.com.br">contato@inovacaointel.com.br</a><br>
+<strong>PARANÁ:</strong> SEC Estratégias, (41) 3019-3717, <a href="mailto:opec@sec-estrategias.com.br">opec@sec-estrategias.com.br</a><br>
+<strong>Gerente de Circulação: </strong>Rita Teixeira <br>
+<strong>Analista de Circulação - Venda Avulsa:</strong> Juliana Augusta     Pelizzon de Morais<br>
+<strong>Assistentes de circulação - Assinaturas: </strong><br>
+Leonardo Borgiani e Luciana Cristina Silva<br>
+<strong>DIRETORA-ADMINISTRATIVA/FINANCEIRA:</strong> Renata Ribeiro dos Santos<br>
+<strong>COORDENADOR ADMINISTRATIVO/FINANCEIRO:</strong> Mario Yamanaka<br>
+<strong>EQUIPE ADMINISTRATIVA e financeira:</strong> Fabiana lopes santos,     Juliana Torres Corrêa<br>
+<strong>Assistente de administrativo e de RH:</strong> Maria Rocha Calixto<br>
+<strong>Controller:</strong> Eduardo Lucchesi<br>
+<strong><br>
+ASSESSORIA CONTÁBIL, FISCAL E TRABALHISTA:</strong><br>
+Firbraz Serviços Contábeis Ltda. <br>
+Av. Pedroso de Moraes, 2219 – Pinheiros – SP/SP – CEP 05419-001 <br>
+<a href="http://www.firbraz.com.br">www.firbraz.com.br</a>, fone (11)3463-6555<br>
+<br>
+CartaCapital é uma publicação semanal da Editora Confiança Ltda.     Carta­Capital não se responsabiliza pelos conceitos emitidos nos     artigos assinados.As pessoas que não constarem do expediente não têm     autorização para falar em nome de CartaCapital ou para retirar     qualquer tipo de material se não possuírem em seu poder carta em     papel timbrado assinada por qualquer pessoa que conste do     expediente. Registro nº 179.584 de 23/8/94, modificado pelo registro     nº 219.316 de 30/4/2002 no 1º Cartório, de acordo com a Lei de     Imprensa. <br>
+Impressão simultânea: PLURAL Editora e Gráfica - São Paulo - SP<br>
+e Recife - PE Distribuição em bancas: Fernando Chinaglia<br>
+Assinantes: Door to Door Logística e Distribuição				</div>
 				<!-- ends floated content -->
 
 				<!-- sidebar -->
